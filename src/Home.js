d52aa810bb17a8da26989af530b9082a277a74ef
Alvin <alvin@Mac.mymeshdevice.home>
2026-08-08 22:33:19 +0800

    Scroll offset

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
     const entertainmentRef = useRef(null);
     const environmentRef = useRef(null);
     const educationRef = useRef(null);
-
+    const offset = 100;
     const scrollToSection = (ref) => {
         ref.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
     };
@@ -50,35 +50,35 @@ const Home = () => {
                 {error && <div>{error}</div>}
                 {isPending && <div>{t('loading')}</div>}
                 
-                <div ref={allNewsRef}>
+                <div ref={allNewsRef} style={{scrollMarginTop: `${offset}px`}}>
                     {blogs && <NewsList blogs={blogs} title={t('allNews') + ":"} />}
                 </div>
                 
-                <div ref={politicsRef}>
+                <div ref={politicsRef} style={{scrollMarginTop: `${offset}px`}}>
                     {blogs && <NewsList blogs={blogs.filter((blog)=>blog.category === "politics")} title={t('politicsNews')} />}
                 </div>
                 
-                <div ref={economyRef}>
+                <div ref={economyRef} style={{scrollMarginTop: `${offset}px`}}>
                     {blogs && <NewsList blogs={blogs.filter((blog)=>blog.category === "economy")} title={t('economyNews')} />}
                 </div>
                 
-                <div ref={technologyRef}>
+                <div ref={technologyRef} style={{scrollMarginTop: `${offset}px`}}>
                     {blogs && <NewsList blogs={blogs.filter((blog)=>blog.category === "technology")} title={t('technologyNews')} />}
                 </div>
                 
-                <div ref={healthRef}>
+                <div ref={healthRef} style={{scrollMarginTop: `${offset}px`}}>
                     {blogs && <NewsList blogs={blogs.filter((blog)=>blog.category === "health")} title={t('healthNews')} />}
                 </div>
                 
-                <div ref={entertainmentRef}>
+                <div ref={entertainmentRef} style={{scrollMarginTop: `${offset}px`}}>
                     {blogs && <NewsList blogs={blogs.filter((blog)=>blog.category === "entertainment")} title={t('entertainmentNews')} />}
                 </div>
                 
-                <div ref={environmentRef}>
+                <div ref={environmentRef} style={{scrollMarginTop: `${offset}px`}}>
                     {blogs && <NewsList blogs={blogs.filter((blog)=>blog.category === "environment")} title={t('environmentNews')} />}
                 </div>
                 
-                <div ref={educationRef}>
+                <div ref={educationRef} style={{scrollMarginTop: `${offset}px`}}>
                     {blogs && <NewsList blogs={blogs.filter((blog)=>blog.category === "education")} title={t('educationNews')} />}
                 </div>
             </div>
